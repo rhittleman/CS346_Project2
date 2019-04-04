6bc32f55983b684eecb06e1ac2c3effc46feb700
rhittleman <rhittleman@email.arizona.edu>
2019-04-04 07:40:19 -0700

    currval

--- demo_users.sql
+++ demo_users.sql
@@ -1,10 +1,15 @@
-INSERT INTO logins (username, password, user_id) VALUES (Ben, Ben123,
-	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
+INSERT INTO users DEFAULT VALUES;
+INSERT INTO logins (username, password, user_id) VALUES (Ben, Ben123, 
+	currval(users_user_id_sequence));
+INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES (Ryan, Ryan123,
-	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
+	currval(users_user_id_sequence));
+INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES (Russ, Russ123,
-	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
+	currval(users_user_id_sequence));
+INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES (Ann, Ann123,
-	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
+	currval(users_user_id_sequence));
+INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES (Tony, Tony123,
-	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
+	currval(users_user_id_sequence));
