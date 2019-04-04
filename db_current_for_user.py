d96a9ca2e0eea43819ce3a70003eb15433c6edf6
rhittleman <rhittleman@email.arizona.edu>
2019-04-04 05:05:33 -0700

    query all current fields for user

---
+++ db_current_for_user.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+def find_user(username, password)
+	# Connect to the database
+	conn = psycopg2.connect(host="cs346proj2db.ctkh18zy1p4k.us-east-1.rds.amazonaws.com" 
+		dbname="cs346proj2db" user="cs346proj2admin" password="proj2pass")
+	# Open a cursor to perform database operations
+	cur = conn.cursor()
+
+	# Query the database
+	cur.execute("SELECT * FROM logins NATURAL JOIN users NATURAL JOIN games NATURAL JOIN rounds WHERE user = %s AND pass = %s;" (username, password))
+	response = cur.fetchone()
+
+	# Close communication with the database
+	cur.close()
+	conn.close()
+
+	return response
