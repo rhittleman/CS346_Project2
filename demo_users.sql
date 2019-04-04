f88418f0fbaa61951fdcf82e7b84ce7748121622
rhittleman <rhittleman@email.arizona.edu>
2019-04-04 07:29:31 -0700

    defualt

--- demo_users.sql
+++ demo_users.sql
@@ -1,10 +1,10 @@
 INSERT INTO logins (username, password, user_id) VALUES (Ben, Ben123,
-	INSERT DEFAULT INTO users RETURNING (user_id));
+	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
 INSERT INTO logins (username, password, user_id) VALUES (Ryan, Ryan123,
-	INSERT DEFAULT INTO users RETURNING (user_id));
+	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
 INSERT INTO logins (username, password, user_id) VALUES (Russ, Russ123,
-	INSERT DEFAULT INTO users RETURNING (user_id));
+	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
 INSERT INTO logins (username, password, user_id) VALUES (Ann, Ann123,
-	INSERT DEFAULT INTO users RETURNING (user_id));
+	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
 INSERT INTO logins (username, password, user_id) VALUES (Tony, Tony123,
-	INSERT DEFAULT INTO users RETURNING (user_id));
+	INSERT INTO users DEFAULT VALUES RETURNING (user_id));
