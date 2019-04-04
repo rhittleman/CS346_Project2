f81a8c4f778f1a9098d5674380e04e205e2a0701
rhittleman <rhittleman@email.arizona.edu>
2019-04-04 07:49:45 -0700

    '

--- demo_users.sql
+++ demo_users.sql
@@ -1,15 +1,15 @@
 INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES ('Ben', 'Ben123', 
-	currval(users_user_id_sequence));
+	currval('users_user_id_sequence'));
 INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES ('Ryan', 'Ryan123',
-	currval(users_user_id_sequence));
+	currval('users_user_id_sequence'));
 INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES ('Russ', 'Russ123',
-	currval(users_user_id_sequence));
+	currval('users_user_id_sequence'));
 INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES ('Ann', 'Ann123',
-	currval(users_user_id_sequence));
+	currval('users_user_id_sequence'));
 INSERT INTO users DEFAULT VALUES;
 INSERT INTO logins (username, password, user_id) VALUES ('Tony', 'Tony123',
-	currval(users_user_id_sequence));
+	currval('users_user_id_sequence'));
