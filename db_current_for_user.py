3a94174eb6638f330ab8f68ba9a585a16f7dfdad
rhittleman <rhittleman@email.arizona.edu>
2019-04-04 05:06:44 -0700

    added header

--- db_current_for_user.py
+++ db_current_for_user.py
@@ -1,5 +1,7 @@
 import psycopg2
 
+# Returns all current fields for a given username and password
+# Returns None if the user is not found
 def find_user(username, password)
 	# Connect to the database
 	conn = psycopg2.connect(host="cs346proj2db.ctkh18zy1p4k.us-east-1.rds.amazonaws.com" 
